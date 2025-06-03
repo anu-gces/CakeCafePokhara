7732005ac8f28ea930f51f8dbd27ef4b97169e83
anu-gces <be2020ce57@gces.edu.np>
2025-06-03 11:50:49 +0545

    Checking notificaiton ssytem

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -19,10 +19,6 @@ const messaging = firebase.messaging()
 
 messaging.onBackgroundMessage(function (payload) {
   // Customize notification here
-  self.registration.showNotification(payload.notification.title, {
-    body: payload.notification.body,
-    icon: '/CakeCafeLogo.png',
-  })
 })
 
 self.addEventListener('notificationclick', function (event) {
