896192af8424956a370b5ce85b40281add1977f3
anu-gces <be2020ce57@gces.edu.np>
2025-06-26 11:22:25 +0545

    failed to prompt

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,6 @@
-import { StrictMode, useState } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {
-  Drawer,
-  DrawerContent,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerFooter,
-} from '@/components/ui/drawer'
 import { AppProvider } from './appProvider'
-import { Button } from './components/ui/button'
-import { registerSW } from 'virtual:pwa-register'
 
 // Register Firebase Messaging Service Worker
 if ('serviceWorker' in navigator) {
@@ -28,51 +19,8 @@ if ('serviceWorker' in navigator) {
   })
 }
 
-export function UpdatePrompt({
-  open,
-  onReload,
-}: {
-  open: boolean
-  onReload: () => void
-}) {
-  return (
-    <Drawer open={open}>
-      <DrawerContent>
-        <DrawerHeader>
-          <DrawerTitle>Update Available</DrawerTitle>
-        </DrawerHeader>
-        <div className="py-2">A new version of Cake Cafe is available.</div>
-        <DrawerFooter>
-          <Button onClick={onReload}>Reload</Button>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
-  )
-}
-
-function Root() {
-  const [showUpdate, setShowUpdate] = useState(false)
-
-  registerSW({
-    onNeedRefresh() {
-      setShowUpdate(true)
-    },
-  })
-
-  const handleReload = () => {
-    window.location.reload()
-  }
-
-  return (
-    <>
-      <AppProvider />
-      <UpdatePrompt open={showUpdate} onReload={handleReload} />
-    </>
-  )
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Root />
+    <AppProvider />
   </StrictMode>,
 )
