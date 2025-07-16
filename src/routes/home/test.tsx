8e46bcc7c41b126de9c0de983da446d672d934c6
anu-gces <be2020ce57@gces.edu.np>
2025-07-17 00:51:16 +0545

    no test

--- src/routes/home/test.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/home/test')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/home/test"!</div>
-}
